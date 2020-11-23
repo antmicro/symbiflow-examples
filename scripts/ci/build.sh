813fed003f9cf13b206e48acc4c704adb55a277a
Michael Gielda <mgielda@antmicro.com>
2020-11-23 11:01:33 +0100

    Rework the scripts to keep a useful build.sh
    Signed-off-by: Michael Gielda <mgielda@antmicro.com>

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -2,20 +2,12 @@
 
 fpga_fam=$1
 
-# create conda env
-eval $(tuttest docs/getting-symbiflow.rst wget-conda)
-eval $(tuttest docs/getting-symbiflow.rst conda-install-dir)
-eval $(tuttest docs/getting-symbiflow.rst fpga-fam-$fpga_fam)
-eval $(tuttest docs/getting-symbiflow.rst conda-setup)
-eval $(tuttest docs/getting-symbiflow.rst download-arch-def-$fpga_fam)
-
 # activate conda and enter example dir
 eval $(tuttest docs/building-examples.rst export-install-dir)
 eval $(tuttest docs/building-examples.rst fpga-fam-$fpga_fam)
 eval $(tuttest docs/building-examples.rst conda-prep-env)
 eval $(tuttest docs/building-examples.rst conda-act-env)
 eval $(tuttest docs/building-examples.rst enter-dir-$fpga_fam)
-
 
 if [ "$fpga_fam" = "xc7" ]; then
     # counter example
---
+++ scripts/ci/install-toolchain.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fpga_fam=$1
+
+# create conda env
+eval $(tuttest docs/getting-symbiflow.rst wget-conda)
+eval $(tuttest docs/getting-symbiflow.rst conda-install-dir)
+eval $(tuttest docs/getting-symbiflow.rst fpga-fam-$fpga_fam)
+eval $(tuttest docs/getting-symbiflow.rst conda-setup)
+eval $(tuttest docs/getting-symbiflow.rst download-arch-def-$fpga_fam)
+
