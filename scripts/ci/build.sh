74df7d6ffd15c8a4f673d4a31685a7639ace66dc
Filip Kokosinski <fkokosinski@antmicro.com>
2020-11-27 11:41:45 +0100

    ci: add xenial

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -11,36 +11,36 @@ if [ "$fpga_fam" == "xc7" -a -z "$examples" ] ; then
 fi;
 
 # activate conda and enter example dir
-eval $(tuttest docs/building-examples.rst export-install-dir)
-eval $(tuttest docs/building-examples.rst fpga-fam-$fpga_fam)
-eval $(tuttest docs/building-examples.rst conda-prep-env)
-eval $(tuttest docs/building-examples.rst conda-act-env)
-eval $(tuttest docs/building-examples.rst enter-dir-$fpga_fam)
+eval $(tuttest docs/building-examples.rst export-install-dir 2>&1)
+eval $(tuttest docs/building-examples.rst fpga-fam-$fpga_fam 2>&1)
+eval $(tuttest docs/building-examples.rst conda-prep-env 2>&1)
+eval $(tuttest docs/building-examples.rst conda-act-env 2>&1)
+eval $(tuttest docs/building-examples.rst enter-dir-$fpga_fam 2>&1)
 
 if [ "$fpga_fam" = "xc7" ]; then
     for example in $examples; do
         case $example in
             "counter")
                 # counter example
-                eval $(tuttest ../docs/building-examples.rst example-counter-a35t)
+                eval $(tuttest ../docs/building-examples.rst example-counter-a35t 2>&1)
                 make -C counter_test clean
-                eval $(tuttest ../docs/building-examples.rst example-counter-a100t)
+                eval $(tuttest ../docs/building-examples.rst example-counter-a100t 2>&1)
                 make -C counter_test clean
-                eval $(tuttest ../docs/building-examples.rst example-counter-basys3)
+                eval $(tuttest ../docs/building-examples.rst example-counter-basys3 2>&1)
                 make -C counter_test clean
                 ;;
             "picosoc")
                 # PicoSoC example
-                eval $(tuttest ../docs/building-examples.rst example-picosoc-basys3) 
+                eval $(tuttest ../docs/building-examples.rst example-picosoc-basys3 2>&1) 
                 ;;
             "litex")
                 # LiteX example
-                eval $(tuttest ../docs/building-examples.rst example-litex-deps)
-                eval $(tuttest ../docs/building-examples.rst example-litex-a35t)
+                eval $(tuttest ../docs/building-examples.rst example-litex-deps 2>&1)
+                eval $(tuttest ../docs/building-examples.rst example-litex-a35t 2>&1)
                 ;;
         esac
     done
 else
     # counter example
-    eval $(tuttest ../docs/building-examples.rst eos-s3-counter)
+    eval $(tuttest ../docs/building-examples.rst eos-s3-counter 2>&1)
 fi;
