a6b97c2f68cec6c873ecdd1ca4508b9e68aa3bed
Filip Kokosinski <fkokosinski@antmicro.com>
2020-12-08 11:46:27 +0100

    wip: fix paths in ci scripts

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -27,16 +27,17 @@ if [ "$fpga_fam" = "xc7" ]; then
                 eval $(tuttest counter_test/README.rst example-counter-a100t-group 2>&1)
                 make -C counter_test clean
                 eval $(tuttest counter_test/README.rst example-counter-basys3-group 2>&1)
-                make -C counter_test clean
                 ;;
             "picosoc")
                 # PicoSoC example
-                eval $(tuttest ../docs/building-examples.rst example-picosoc-basys3 2>&1) 
+                eval $(tuttest picosoc_demo/README.rst example-picosoc-basys3-group 2>&1)
                 ;;
             "litex")
                 # LiteX example
-                eval $(tuttest ../docs/building-examples.rst example-litex-deps 2>&1)
-                eval $(tuttest ../docs/building-examples.rst example-litex-a35t 2>&1)
+                eval $(tuttest xc7/linux_litex_demo/README.rst example-litex-deps 2>&1)
+                eval $(tuttest xc7/linux_litex_demo/README.rst example-litex-a35t-group 2>&1)
+                make -C linux_litex_demo
+                eval $(tuttest xc7/linux_litex_demo/README.rst example-litex-a100t-group 2>&1)
                 ;;
         esac
     done
